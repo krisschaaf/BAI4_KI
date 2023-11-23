b5d51f2b2cf35fb1acc74b777e65c6ea872d2830
krisschaaf <kris-schaaf@gmx.de>
2023-11-23 15:28:40 +0100

    policy iteration taxi and frozen lake

--- Praktikum_2/src/q_learning/taxi.py
+++ Praktikum_2/src/q_learning/taxi.py
@@ -3,6 +3,7 @@ import gym
 import numpy as np
 from gym import *
 import random
+from IPython.display import clear_output
 
 
 def q_learning(_env, _qtable, _num_iterations, _epsilon, _discount_rate, _learning_rate, _decay_rate):
@@ -86,6 +87,50 @@ def watchTrainedAgent(_num_iterations, _qtable):
     np.set_printoptions(threshold=sys.maxsize)
 
 
+def calculatePathByOptimalPolicy(taxi_row, taxi_column, passenger_location, destination, _optimal_policy, _env):
+    passenger_matrix = np.array([[0, 0], [0, 4], [4, 0], [4, 3]])
+    pathByOptimalPolicy = []
+    _env.reset()
+
+    while True:
+        state = _env.encode(taxi_row, taxi_column, passenger_location, destination)
+        action = np.argmax(_optimal_policy[state])
+        pathByOptimalPolicy.append(state)
+        _env.s = state
+        p = _env.render()
+        print(p)
+        match action:
+            case 0:
+                taxi_column += 1
+            case 1:
+                taxi_column -= 1
+            case 2:
+                taxi_row += 1
+            case 3:
+                taxi_row -= 1
+            case 4:
+                if passenger_location == 4:
+                    raise Exception("Taxi trying to pick up passenger who is already inside the car.")
+
+                if np.array_equal(passenger_matrix[passenger_location], [taxi_row, taxi_column]):
+                    passenger_location = 4
+                else:
+                    raise Exception("Taxi trying to pick up passenger who is not on the designated pick up field.")
+            case 5:
+                if passenger_location != 4:
+                    raise Exception("Taxi trying to drop off passenger who is not inside the car.")
+
+                if not np.array_equal([taxi_row, taxi_column], passenger_matrix[destination]):
+                    state = _env.encode(taxi_row, taxi_column, passenger_location, destination)
+                    pathByOptimalPolicy.append(state)
+
+                    break
+                else:
+                    raise Exception("Taxi trying to drop off passenger at the wrong destination.")
+
+    return pathByOptimalPolicy
+
+
 if __name__ == "__main__":
     env: gym.Env = gym.make('Taxi-v3', render_mode="ansi")
     # hyperparameters
@@ -104,4 +149,7 @@ if __name__ == "__main__":
 
 
     print("\r\nOptimal Policy:")
-    # print(qtable)
+    print(qtable)
+
+    path = calculatePathByOptimalPolicy(0, 0, 0, 2, qtable, env)
+    print(path)
--- Praktikum_2/src/value_iteration/taxi.py
+++ Praktikum_2/src/value_iteration/taxi.py
@@ -1,4 +1,3 @@
-import sys
 import numpy as np
 import gym
 
@@ -8,35 +7,88 @@ def value_iteration(_env, _num_iterations, _epsilon, _discount_rate):
     action_size = _env.action_space.n
 
     # initialize value function
-    optimal_policy = np.zeros(state_size)
+    V = np.zeros(state_size)
 
     for _ in range(_num_iterations):
         delta = 0
 
         for state in range(state_size):
-            current_state = optimal_policy[state]
-            action_values = []
+            current_state = V[state]
+            A = np.zeros(action_size)
 
             for action in range(action_size):
-                state_value = 0
                 for probability, next_state, reward, _ in _env.P[state][action]:
-                    state_action_value = probability * (reward + _discount_rate * optimal_policy[next_state])
-                    state_value += state_action_value
-                action_values.append(state_value)
+                    A[action] += probability * (reward + _discount_rate * V[next_state])
 
-            optimal_policy[state] = max(action_values)
+            V[state] = max(A)
 
-            delta = max(delta, abs(current_state - optimal_policy[state]))
+            delta = max(delta, abs(current_state - V[state]))
 
         # delta converges to epsilon -> stop
         if delta < _epsilon:
             break
 
-    return optimal_policy
+    policy = np.zeros([state_size, action_size])
+
+    for state in range(state_size):  # for all states, create deterministic policy
+
+        A = np.zeros(action_size)
+        for action in range(action_size):
+            for probability, next_state, reward, _ in _env.P[state][action]:
+                A[action] += probability * (reward + _discount_rate * V[next_state])
+
+        best_action = np.argmax(A)
+        policy[state][best_action] = 1
+
+    return V, policy
+
+
+def calculatePathByOptimalPolicy(taxi_row, taxi_column, passenger_location, destination, _optimal_policy, _env):
+    passenger_matrix = np.array([[0, 0], [0, 4], [4, 0], [4, 3]])
+    pathByOptimalPolicy = []
+    _env.reset()
+
+    while True:
+        state = _env.encode(taxi_row, taxi_column, passenger_location, destination)
+        action = np.argmax(_optimal_policy[state])
+        pathByOptimalPolicy.append(state)
+        _env.s = state
+        p = _env.render()
+        print(p)
+        match action:
+            case 0:
+                taxi_column += 1
+            case 1:
+                taxi_column -= 1
+            case 2:
+                taxi_row += 1
+            case 3:
+                taxi_row -= 1
+            case 4:
+                if passenger_location == 4:
+                    raise Exception("Taxi trying to pick up passenger who is already inside the car.")
+
+                if np.array_equal(passenger_matrix[passenger_location], [taxi_row, taxi_column]):
+                    passenger_location = 4
+                else:
+                    raise Exception("Taxi trying to pick up passenger who is not on the designated pick up field.")
+            case 5:
+                if passenger_location != 4:
+                    raise Exception("Taxi trying to drop off passenger who is not inside the car.")
+
+                if not np.array_equal([taxi_row, taxi_column], passenger_matrix[destination]):
+                    state = _env.encode(taxi_row, taxi_column, passenger_location, destination)
+                    pathByOptimalPolicy.append(state)
+
+                    break
+                else:
+                    raise Exception("Taxi trying to drop off passenger at the wrong destination.")
+
+    return pathByOptimalPolicy
 
 
 if __name__ == "__main__":
-    env = gym.make('Taxi-v3')
+    env = gym.make('Taxi-v3', render_mode='ansi')
 
     # value iteration hyperparameters
     num_iterations = 10000
@@ -44,7 +96,13 @@ if __name__ == "__main__":
     discount_rate = 0.8
 
     # perform value iteration
-    optimal_policy = value_iteration(env, num_iterations, epsilon, discount_rate)
+    V, optimal_policy = value_iteration(env, num_iterations, epsilon, discount_rate)
+
+    print("Optimal Values:")
+    print(V)
 
     print("Optimal Policy:")
     print(optimal_policy)
+
+    path = calculatePathByOptimalPolicy(0, 0, 0, 2, optimal_policy, env)
+    print(path)
