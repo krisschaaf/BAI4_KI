966d65b284e28d94e5ac77dc755c7716e12127fb
krisschaaf <kris-schaaf@gmx.de>
2023-12-14 09:26:04 +0100

    finish excercise 1 with questions

--- Praktikum_3/src/1/aufgabe_1.py
+++ Praktikum_3/src/1/aufgabe_1.py
@@ -1,17 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from sympy import cos, sin
 
 
 def loss_function(_w1, _w2, _x1, _x2, _y_tar):
     # Fehlerfunktion E aus Aufgabenzettel (Mittlere quadratische Abweichung/Fehler)
-    sinval = np.sin(_w1 * _x1)
-    cosval = np.cos(_w2 * _x2)
-
-    return (1 / 2) * (sinval + cosval + _w2 - _y_tar) ** 2
+    return (1 / 2) * (np.sin(_w1 * _x1) + np.cos(_w2 * _x2) + _w2 - _y_tar) ** 2
 
 
-def visualize(current_w1, current_w2, _x1, _x2, _y_tar, title):
+def visualize(_current_w1, _current_w2, _x1, _x2, _y_tar, title):
     _x = np.linspace(-10, 10, 100)  # Der fuer diese Aufgabe interessante Definitionsbereich liegt zwischen -10 und 10
     y = np.linspace(-10, 10, 100)
     X, Y = np.meshgrid(_x, y)
@@ -23,8 +19,12 @@ def visualize(current_w1, current_w2, _x1, _x2, _y_tar, title):
     ax = plt.axes(projection='3d')
     ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap='jet', edgecolor='none')  # Fehlerfunktion im 3D Raum darstellen
 
-    current_loss = loss_function(float(current_w1), float(current_w2), _x1, _x2, _y_tar)
-    ax.plot(current_w1, current_w2, current_loss, marker="o",
+    _current_loss = loss_function(float(_current_w1), float(_current_w2), _x1, _x2, _y_tar)
+
+    if title == "Fehlergebirge - Start":
+        print('Fehlerwert Start: ' + str(round(_current_loss, 1)) + '\r\n')
+
+    ax.plot(_current_w1, _current_w2, _current_loss, marker="o",
             markersize=10, markeredgecolor="black", markerfacecolor="white")  #
 
     ax.set_title(title, fontsize=13)
@@ -34,7 +34,7 @@ def visualize(current_w1, current_w2, _x1, _x2, _y_tar, title):
 
     plt.show()
 
-    return current_w1, current_w2, current_loss
+    return _current_w1, _current_w2, _current_loss
 
 
 if __name__ == '__main__':
@@ -57,18 +57,29 @@ if __name__ == '__main__':
     x2 = 1.5
     y_tar = 2.0
 
-    visualize(current_w1=w1, current_w2=w2, _x1=x1, _x2=x2, _y_tar=y_tar, title="Fehlergebirge - Start")
+    visualize(_current_w1=w1, _current_w2=w2, _x1=x1, _x2=x2, _y_tar=y_tar, title="Fehlergebirge - Start")
 
     # Schrittweise Näherung des/eines Minimums
     for x in range(1000):
-        # w1 = w1 - alpha * (x1 * (w2 - y_tar + sin(w1 * x1) + cos(w2 * x2)) * cos(w1 * x1))
-        w1 = w1 - alpha *(x1 * cos(x1 * w1) * (sin(x1 * w1) - y_tar + cos(w2 * x2) + w2))
-        # w2 = w2 - alpha * (0.5 * (-2 * x2 * sin(w2 * x2) + 2) * (w2 - y_tar + sin(w1 * x1) + cos(w2 * x2)))
-        w2 = w2 - alpha * (cos(x2 * w2) + w2 - y_tar + sin(w1 * x1)) * (1 - x2 * sin(x2 * w2))
+        w1 = w1 - alpha * (x1 * np.cos(x1 * w1) * (np.sin(x1 * w1) - y_tar + np.cos(w2 * x2) + w2))
+        w2 = w2 - alpha * (np.cos(x2 * w2) + w2 - y_tar + np.sin(w1 * x1)) * (1 - x2 * np.sin(x2 * w2))
 
-    print('w1 Start: ' + str(w1_start) + '; w1 Ende: ' + str(round(w1, 1)))
-    print('w2 Start: ' + str(w2_start) + '; w2 Ende: ' + str(round(w2, 1)))
+    print('w1 Start (Vorhersage): ' + str(w1_start) + '; w1 Ende: ' + str(round(w1, 1)))
+    print('w2 Start (Vorhersage): ' + str(w2_start) + '; w2 Ende: ' + str(round(w2, 1)) + '\r\n')
 
-    current_w1, current_w2, current_loss = visualize(current_w1=w1, current_w2=w2, _x1=x1, _x2=x2, _y_tar=y_tar, title="Fehlergebirge - Ende")
-    print('x: ' + str(round(current_w1, 1)) + ' y: ' + str(round(current_w2, 1)) + ' z: ' + str(round(current_loss, 1)))
+    current_w1, current_w2, current_loss = visualize(_current_w1=w1, _current_w2=w2, _x1=x1, _x2=x2, _y_tar=y_tar,
+                                                     title="Fehlergebirge - Ende")
+    print('x: ' + str(round(current_w1, 1)) + ' y: ' + str(round(current_w2, 1)) + ' z: ' + str(round(current_loss, 1))
+          + ' (Fehlerwert)')
 
+# Um schrittweise das Minimum der Funktion zu ermitteln, sind die partiellen Ableitungen
+# nach den lernbaren Gewichten notwendig (warum?).
+# → Um das lokale Minimum zu ermitteln, müssen wir die Fehlerfunktion entlang der stärksten Steigung nach unten
+# 'fahren'. Anhand der ersten Ableitung kann die Steigung ermittelt werden.
+
+# Wie unterscheiden sich Fehlerwert, Gewichtswerte und Vorhersagen?
+# → Der Fehlerwert wird kleiner (nähert sich gegen 0)
+# → Die Gewichtswerte passen sich an das lokale Minimum an
+# → Siehe Console
+
+# → Auffällig ist, dass obwohl das gleiche Datensample genutzt wird, verschiedene lokale Minima gefunden werden
